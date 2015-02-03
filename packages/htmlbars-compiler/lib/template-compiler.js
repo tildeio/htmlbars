7d7aa98325a1071e5bd49e0c57a3605b551e8a0e
Tom Dale and Yehuda Katz <engineering+tomdale+wycats@tilde.io>
2015-02-02 18:51:32 -0800

    Encapsulate state needed for re-render
    This commit introduces the notion of a “result” data type, which
    encapsulates all of the state needed to be passed to a compiled template
    in order to successfully re-render with an existing element.
    
    Rather than returning a document fragment, `render()` now returns this
    result type. For compatibility, consumers of this API will need to
    use the `fragment` property of the result rather than the result
    directly.
    
    In order to re-render, you can pass the last result as the `lastResult`
    option, and it will use that element and morph to update.
    
    One small additional change: contextual elements must now be passed in
    the options hash. We decided to make this change as the list of
    arguments was growing unwieldy, and we wanted to make this durable to
    changes in the future. We will also investigate incorporating block
    params into the options hash.

--- packages/htmlbars-compiler/lib/template-compiler.js
+++ packages/htmlbars-compiler/lib/template-compiler.js
@@ -83,7 +83,7 @@ TemplateCompiler.prototype.endProgram = function(program, programDepth) {
 
   var blockParams = program.blockParams || [];
 
-  var templateSignature = 'context, env, contextualElement';
+  var templateSignature = 'context, env, options';
   if (blockParams.length > 0) {
     templateSignature += ', blockArguments';
   }
@@ -100,8 +100,11 @@ TemplateCompiler.prototype.endProgram = function(program, programDepth) {
     indent+'    render: function render(' + templateSignature + ') {\n' +
     indent+'      var dom = env.dom;\n' +
     this.getHydrationHooks(indent + '      ', this.hydrationCompiler.hooks) +
+    indent+'      var contextualElement = options && options.contextualElement;\n' +
+    indent+'      var lastResult = options && options.lastResult;\n' +
     indent+'      dom.detectNamespace(contextualElement);\n' +
-    indent+'      var fragment = env.target;\n' +
+    indent+'      var fragment = lastResult ? lastResult.fragment : null;\n' +
+    indent+'      var morphs = lastResult ? lastResult.morphs : null;\n' +
     indent+'      if (!fragment && env.useFragmentCache && dom.canClone) {\n' +
     indent+'        if (this.cachedFragment === null) {\n' +
     indent+'          fragment = this.build(dom);\n' +
@@ -118,7 +121,7 @@ TemplateCompiler.prototype.endProgram = function(program, programDepth) {
     indent+'        fragment = this.build(dom);\n' +
     indent+'      }\n' +
     hydrationProgram +
-    indent+'      return { fragment: fragment, morphs: morphs };\n' +
+    indent+'      return lastResult || { fragment: fragment, morphs: morphs };\n' +
     indent+'    }\n' +
     indent+'  };\n' +
     indent+'}())';
